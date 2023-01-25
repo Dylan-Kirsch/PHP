e761ee26d34a67ceb6db65840c7e341dfde9b87c
Dylan-Kirsch <kirsch-dylan@bbox.fr>
2023-01-25 16:20:07 +0100

    php tableau

---
+++ découvertePHP/serie-03/tp1.php
@@ -0,0 +1,42 @@
+<?php
+
+    // tableau simple : 
+    
+    echo $tableau[0] = 2023;
+    echo $tableau[1] = "DEVWEB-18-S2";
+    echo $tableau[2] = 11.12;
+
+    // tableau à 2 dimensions : 
+    echo $tab[0][0] = 12;
+    echo $tab[0][1] = "tv";
+    echo $tab[1][0] = 1245.678;
+    echo $tab[1][1] = "bye";
+
+    // tableau associatif : 
+    echo $toto["Nom"] = "le nom de famille de Toto...";
+    echo $toto["Age"] = 12;
+    echo $toto["Adresse"] = "22 rue des bois fleuri";
+
+    // $tableau = array(
+    //     0     => 2023,
+    //     1     => "DEVWEB-18-S2",
+    //     -100  => 100,
+    // );
+    // var_dump($tableau);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
